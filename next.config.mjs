27302d3d515f6b4a3edfbac3aa875f12517acb63
evcorona <eldav.corona@gmail.com>
2024-09-07 06:11:31 -0600

    Add envs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: { URL_API: process.env.URL_API, NEXT_ENV: process.env.NEXT_ENV },
+};
 
 export default nextConfig;
